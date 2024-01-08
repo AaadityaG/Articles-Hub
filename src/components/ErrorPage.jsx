4fb6a6a7afb2a3048c28a46aa00373c2f75002fc
Aditya <helloadig@gmail.com>
2024-01-08 17:25:43 +0530

    minor updates

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,22 +5,22 @@ function ErrorPage() {
     // <div>
     //   <h1>This is Error 404</h1>
     // </div>
-    <div className="min-h-screen flex items-center justify-center m-4">
+    <div className="min-h-screen flex items-center justify-center backdrop-blur-lg">
       <div className="max-w-md ">
         <img
-          src="https://illustrations.popsy.co/amber/crashed-error.svg"
+          src="https://illustrations.popsy.co/red/crashed-error.svg"
           alt="Error Illustration"
           className="mb-8 mx-auto"
         />
-        <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+        <h1 className="text-3xl font-semibold text-gray-200 mb-4">
           Oops! Something went wrong.
         </h1>
-        <p className="text-gray-600 mb-8">
+        <p className="text-gray-200 mb-8">
           We apologize for the inconvenience. Please try again later.
         </p>
         <button
           onClick={() => window.history.back()}
-          className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-400 focus:outline-none focus:ring focus:border-blue-300"
+          className="bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-400 focus:outline-none focus:ring focus:border-blue-300"
         >
           Go Back
         </button>
--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import Signup from './pages/Signup'
 import EditPost from "./pages/EditPost";
 import Post from "./pages/Post";
 import AllPosts from "./pages/AllPosts";
+// import Profile from './pages/Profile.jsx'
 
 const router = createBrowserRouter([
   {
